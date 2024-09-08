c3b8354dda771a639161a4002d126511b899e388
Mirembe Peace Mercy <peace2de@gmail.com>
2024-09-08 19:52:14 +0300

    lesson 3 of javascript, objects

---
+++ lesson.js
@@ -0,0 +1,10 @@
+// Object in javascript
+let pallet  ={
+    randomColors : ['green', 'orange', 'purple'],
+    name : 'canvas'
+
+};
+
+console.log(pallet.randomColors);
+console.log(pallet.name);
+
